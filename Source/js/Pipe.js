6197684d28a228c07b9ce3dff9fd42474917ce56
soer7022 <soer7033@gmail.com>
2017-12-01 14:44:10 +0100

    First Real Commit[M _7:

---
+++ Source/js/Pipe.js
@@ -0,0 +1,42 @@
+function Pipe() {
+    this.top = random(height / 2);
+    this.gap = random(height / 2);
+    this.bottom = (this.top + this.gap) - this.height;
+    this.x = width;
+    this.w = 20;
+    this.speed = 5;
+    this.highlight = false;
+    this.score = 0;
+    this.hasBeenHit = false;
+
+      this.hits = function (bird) {
+        if(bird.x > this.x && bird.x < this.x + this.w) {
+            if(bird.y < this.top || bird.y > height - this.bottom) {
+                this.highlight = true;
+                return 1;
+            } else {
+                if(this.hasBeenHit == false){
+                    this.hasBeenHit = true;
+                    return 2;
+                }
+                return 0;
+            }
+        }
+        this.highlight = false;
+        return 0;
+    }
+
+    this.show = function () {
+        fill(255);
+        if(this.highlight) {
+            fill(255,0,0);
+        }
+        rect(this.x, 0, this.w, this.top);
+        rect(this.x,height-this.bottom,this.w,this.bottom);
+    }
+
+    this.update = function () {
+        this.x -= this.speed;
+
+    }
+}
---
+++ Source/js/sketch.js
@@ -0,0 +1,49 @@
+var bird;
+var pipes = [];
+var score = 0;
+var highScore = 0;
+
+function setup() {
+    createCanvas(400, 600);
+    bird = new Bird();
+    pipes.push(new Pipe());
+}
+
+function draw() {
+    background(0);
+    bird.show();
+    bird.update();
+
+    for(var i = pipes.length-1; i >= 0; i--) {
+        pipes[i].show();
+        pipes[i].update();
+
+        var hit = pipes[i].hits(bird);
+
+        if (hit == 1) {
+            pipes.length = 1;
+            score = 0;
+        }
+        if (hit == 2) {
+            score++;
+            console.log("Not hit")
+        }
+
+        if(pipes[i].x < 0) {
+            pipes.splice(i,1)
+        }
+    }
+
+    if(frameCount % 60 == 0) {
+        pipes.push(new Pipe());
+    }
+
+    highScore = Math.max(score,highScore);
+    document.getElementById("score").innerHTML = "Score: " + score + " High score: " + highScore;
+}
+
+function keyPressed() {
+    if(key == ' ') {
+        bird.up();
+    }
+}
